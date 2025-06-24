66f83f73eafac7ede2e06e32b3ccef59ff4a58b3
FedericoHerreraa <fede.juan.herrera@gmail.com>
2025-06-24 08:03:30 -0300

    Initial Commit

---
+++ DailyFlowApp/DailyFlowAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyFlowAppApp.swift
+//  DailyFlowApp
+//
+//  Created by Federico Herrera on 24/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct DailyFlowAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
